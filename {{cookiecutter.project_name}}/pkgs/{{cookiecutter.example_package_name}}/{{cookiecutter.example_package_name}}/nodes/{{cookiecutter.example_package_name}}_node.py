15a26d860300cdb0c6e4d612198d30523fd00cd9
Alex Thiele <alex@urbanmachine.build>
2024-11-19 13:38:58 -0800

    Fix various linting errors

--- {{cookiecutter.project_name}}/pkgs/{{cookiecutter.example_package_name}}/{{cookiecutter.example_package_name}}/nodes/{{cookiecutter.example_package_name}}_node.py
+++ {{cookiecutter.project_name}}/pkgs/{{cookiecutter.example_package_name}}/{{cookiecutter.example_package_name}}/nodes/{{cookiecutter.example_package_name}}_node.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from node_helpers.nodes import HelpfulNode
 from pydantic import BaseModel
 from std_msgs.msg import String
@@ -17,13 +19,15 @@ class {{cookiecutter.example_node_name}}(HelpfulNode):
         self.params = self.declare_from_pydantic_model(self.Parameters, "config")
 
         # Create a publisher
-        self.publisher = self.create_publisher(String, "example_topic", qos_profile=qos_profile_services_default)
+        self.publisher = self.create_publisher(
+            String, "example_topic", qos_profile=qos_profile_services_default
+        )
 
         # Create a timer
         self.create_timer(1 / self.params.publish_hz, self.on_publish)
 
 
-    def on_publish(self):
+    def on_publish(self) -> None:
         msg = String()
         msg.data = self.params.publish_value
         self.publisher.publish(msg)
